232a4c7d2ebfbe0a34f9a192b6d8608b57c1223b
Benjamin Hemingway <uctpbhe@ucl.ac.uk>
2017-11-11 03:33:46 +0000

    Iterators.jl now IterTools.jl

--- src/Smolyak.jl
+++ src/Smolyak.jl
@@ -29,7 +29,7 @@ typealias ScalarOrVec{T} Union{T,Vector{T}}
 """
 module Smolyak
 
-using Base.Cartesian, Base.LinAlg, Iterators
+using Base.Cartesian, Base.LinAlg, IterTools
 import Base.show 
 
 typealias AA{T} Array{Array{T,1},1}
